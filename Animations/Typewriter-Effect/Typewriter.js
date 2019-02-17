a9de8320aed38793ee3f585986f11c4ff75b83dc
Esten <estengrove99@gmail.com>
2019-02-16 21:00:30 -0700

    Added comments to 'Typewriter-Effect' files.

---
+++ Animations/Typewriter-Effect/Typewriter.js
@@ -0,0 +1,14 @@
+/******************************************
+ *  Author : Esten Grove   
+ *  Created On : Sat Feb 16 2019
+ *  File : Typewriter.js
+ *******************************************/
+
+// Add 3d Hover Effect
+const TypewriterText = document.querySelector('.typewriter');
+
+const hoverZone = document.querySelector('.header-wrapper');
+
+hoverZone.addEventListener('mousemove', e => {
+
+})
