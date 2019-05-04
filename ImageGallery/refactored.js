eb30092d061d9325df46aa4a580779f0fbd7525d
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-05-03 23:03:12 -0700

    Add files via upload

---
+++ ImageGallery/refactored.js
@@ -0,0 +1,18 @@
+/******************************************
+ *  Author : Esten Grove
+ *  Created On : Fri May 03 2019
+ *  File : refactored.js
+ *******************************************/
+// All images in gallery
+const images = document.querySelectorAll('[id^="img-"]');
+
+// Main image display
+const imageBox = document.querySelector(".main-img");
+
+// Add event listener to each gallery image
+// When clicked make that image the src of the main imageBox
+[...images].forEach((img, index) => {
+  img.addEventListener("click", e => {
+    imageBox.setAttribute("src", e.target.src);
+  });
+});
